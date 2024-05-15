d224707e508209ce822ae8726047ee3943083d51
Eber Vladimir <eber.paz9135@alumnos.udg.mx>
2024-05-15 09:27:57 -0600

    Testing

--- CookIt/CookIt/settings.py
+++ CookIt/CookIt/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
